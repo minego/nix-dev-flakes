30ad0fcf8a759919978e0b54f1ed6f16d0a46c1a
Micah N Gorrell <m@minego.net>
2023-11-28 10:53:03 -0700

    Cleanup formatting

--- vaas/flake.nix
+++ vaas/flake.nix
@@ -38,60 +38,60 @@
 				];
 
 				awsconfigskel = builtins.toFile "aws_config_file" ''
-[default]
-output = json
-region = ${AWS_REGION}
+                    [default]
+                    output = json
+                    region = ${AWS_REGION}
 
-[profile trustnet-dev]
-sso_session = vaas
-sso_account_id = 497086895112
-sso_role_name = Vaas.Developer
-region = ${AWS_REGION}
-output = json
+                    [profile trustnet-dev]
+                    sso_session = vaas
+                    sso_account_id = 497086895112
+                    sso_role_name = Vaas.Developer
+                    region = ${AWS_REGION}
+                    output = json
 
-[sso-session vaas]
-sso_start_url = https://d-926708eb5a.awsapps.com/start#/
-sso_region = ${AWS_REGION}
-sso_registration_scopes = sso:account:access
-'';
+                    [sso-session vaas]
+                    sso_start_url = https://d-926708eb5a.awsapps.com/start#/
+                    sso_region = ${AWS_REGION}
+                    sso_registration_scopes = sso:account:access
+                    '';
 
 				shellHook = ''
-export VAAS_HOME=$(pwd)/.vaas_home
-mkdir -p $VAAS_HOME
+                    export VAAS_HOME=$(pwd)/.vaas_home
+                    mkdir -p $VAAS_HOME
 
 
-# Make wrapped copies of specific binaries that will run with their $HOME
-# environment variable overwritten to be a subdir of this environment
-rm -rf $VAAS_HOME/bin
-mkdir $VAAS_HOME/bin
-export PATH="$VAAS_HOME/bin:$PATH"
+                    # Make wrapped copies of specific binaries that will run with their $HOME
+                    # environment variable overwritten to be a subdir of this environment
+                    rm -rf $VAAS_HOME/bin
+                    mkdir $VAAS_HOME/bin
+                    export PATH="$VAAS_HOME/bin:$PATH"
 
-cp `which aws` $VAAS_HOME/bin/
-wrapProgram $VAAS_HOME/bin/aws --set HOME "$VAAS_HOME"
+                    cp `which aws` $VAAS_HOME/bin/
+                    wrapProgram $VAAS_HOME/bin/aws --set HOME "$VAAS_HOME"
 
-cp `which kubectl` $VAAS_HOME/bin/
-wrapProgram $VAAS_HOME/bin/kubectl --set HOME "$VAAS_HOME"
+                    cp `which kubectl` $VAAS_HOME/bin/
+                    wrapProgram $VAAS_HOME/bin/kubectl --set HOME "$VAAS_HOME"
 
-cp `which k9s` $VAAS_HOME/bin/
-wrapProgram $VAAS_HOME/bin/k9s --set HOME "$VAAS_HOME"
+                    cp `which k9s` $VAAS_HOME/bin/
+                    wrapProgram $VAAS_HOME/bin/k9s --set HOME "$VAAS_HOME"
 
 
-# Setup the base AWS configuration in the local dir based on the skeleton
-mkdir -p $VAAS_HOME/.aws
-rm -f $VAAS_HOME/.aws/config
-cp ${awsconfigskel} $VAAS_HOME/.aws/config
+                    # Setup the base AWS configuration in the local dir based on the skeleton
+                    mkdir -p $VAAS_HOME/.aws
+                    rm -f $VAAS_HOME/.aws/config
+                    cp ${awsconfigskel} $VAAS_HOME/.aws/config
 
-# Test AWS access
-aws sts get-caller-identity --profile ${AWS_PROFILE} --no-cli-pager >/dev/null 2>&1
-if [[ $? -eq 0 ]]; then
-	echo "Already logged into AWS"
-else
-	echo "Logging in to AWS..."
-	aws sso login --profile ${AWS_PROFILE}
-	aws eks --region ${AWS_REGION} update-kubeconfig --name dev01 --role-arn arn:aws:iam::497086895112:role/eks/dev01-KubernetesDevelopers
-fi
-kubectl config set-context --current --namespace=$DEVSTACK
-'';
+                    # Test AWS access
+                    aws sts get-caller-identity --profile ${AWS_PROFILE} --no-cli-pager >/dev/null 2>&1
+                    if [[ $? -eq 0 ]]; then
+	                    echo "Already logged into AWS"
+                    else
+	                    echo "Logging in to AWS..."
+	                    aws sso login --profile ${AWS_PROFILE}
+	                    aws eks --region ${AWS_REGION} update-kubeconfig --name dev01 --role-arn arn:aws:iam::497086895112:role/eks/dev01-KubernetesDevelopers
+                    fi
+                    kubectl config set-context --current --namespace=$DEVSTACK
+                    '';
 			};
 		}
 	);
